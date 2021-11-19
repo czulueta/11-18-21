db35e6a045c5e4c6e059e89bea16c14e8024c706
Cesar Zulueta <cesarzulueta12@gmail.com>
2021-11-18 17:49:37 -0800

    built complete frontend and backend todo ap with all packages: mongoose, mongodb, express, express-jwt, jsonwebtoken, dotenv, morgan, bcrypt, react-router, axios, and react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react"
+import Navbar from "./components/Navbar.js"
+import Profile from "./components/Profile.js"
+import Public from "./components/Public.js"
+import Auth from "./components/Auth.js"
+import ProtectedRoute from "./components/ProtectedRoute.js"
+import { UserContext } from "./context/UserProvider.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const { token, logout } = useContext(UserContext)
+  return(
+    <div>
+      { token && <Navbar logout={logout}/>}
+      <Switch>
+        <Route 
+          path="/"
+          render={() => token ? <Redirect to="/profile"/> : <Auth />}
+        />
+        <ProtectedRoute
+          path="/profile"
+          component={Profile}
+          redirectTo="/"
+          token={token} />
+        <ProtectedRotue
+          path="/public"
+          component={Public}
+          redirectTo="/"
+          token={token} />
+      </Switch>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/Public.js
@@ -0,0 +1,9 @@
+import React from "react" 
+
+export default function Public(){
+  return(
+    <div>
+      <h1>Welcome to Your Public Page!!!</h1>
+    </div>
+  )
+}
